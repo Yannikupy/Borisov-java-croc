a49660be4a001ac0530bae4e526004e52ec5ae47
Yanniku <borisov-yan@list.ru>
2021-10-22 23:42:18 +0300

    fixed task3 from week1

--- week1/src/ru/croc/task3/src/Main/Main.java
+++ week1/src/ru/croc/task3/src/Main/Main.java
@@ -12,32 +12,9 @@ public class Main {
         int[] numbers = new int[str.length];
         for(int i = 0; i < str.length; ++i)
         {
-
             numbers[i] = Integer.parseInt(str[i]);
-
         }
-        int min = 0, max = 0, temp_min = 0, temp_max = 0;
-        int[] numbers2 = new int [numbers.length];
-        for(int i = 0; i < numbers.length; ++i){
-
-            numbers2[i] = numbers[i];
-
-        }
-        Arrays.sort(numbers2);
-        min = numbers2[0];
-        max = numbers2[numbers2.length - 1];
-        for(int i = 0; i < numbers.length; ++i){
-
-            if(numbers[i] == min) temp_min = i;
-            if(numbers[i] == max) temp_max = i;
-
-        }
-        int temp_num1 = numbers[0];
-        int temp_num2 = numbers[numbers.length - 1];
-        numbers[0] = min;
-        numbers[temp_min] = temp_num1;
-        numbers[numbers.length - 1] = max;
-        numbers[temp_max] = temp_num2;
+        Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
 
     }
