2dcb8c1bdb769b68ddf9ee1002cce62efb4e612e
Yanniku <borisov-yan@list.ru>
2021-11-16 18:06:31 +0300

    Fixed task11

--- week5/src/ru/croc/task11/src/Lot.java
+++ week5/src/ru/croc/task11/src/Lot.java
@@ -1,19 +1,22 @@
 import java.util.Date;
 
 public class Lot {
-    public int current_price;
-    public String username;
+    public volatile int current_price;
+    public volatile String username;
     public Date end_of_bid;
-    public Date curr_date;
 
-    public synchronized void bet(int new_price, String new_username){
-        if((curr_date = new Date()).before(end_of_bid)  && new_price > current_price){
-            current_price = new_price;
-            username = new_username;
+    public void bet(int new_price, String new_username){
+        if((new Date()).before(end_of_bid)  && new_price > current_price){
+            synchronized (this) {
+                if ((new Date()).before(end_of_bid) && new_price > current_price) {
+                    current_price = new_price;
+                    username = new_username;
+                }
+            }
         }
     }
     public synchronized String getWinnerUsername() throws NoWinnerException {
-        if((curr_date = new Date()).after(end_of_bid)){
+        if((new Date()).after(end_of_bid)){
             return username;
         }
         throw new NoWinnerException("Bidding is live");
