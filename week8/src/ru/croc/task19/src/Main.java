53891b289aa8f46883a796f7d3f6e44e179a3ec7
Yanniku <borisov-yan@list.ru>
2021-11-29 21:38:21 +0300

    fixed task19

--- week8/src/ru/croc/task19/src/Main.java
+++ week8/src/ru/croc/task19/src/Main.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String str = "Hello, World!";
-        BufferedWriter writer = new BufferedWriter(new FileWriter("Hello world!", false));
-        writer.append(str);
-        writer.close();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Hello world.txt", false))) {
+            writer.append(str);
+        }
+        catch (IOException e){
+            System.out.println("Ошибка при работе с файлом");
+        }
     }
 }
